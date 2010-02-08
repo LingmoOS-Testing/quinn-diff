c9a2d99276f6eb886bcad440cb57cdffcad7edfc
Philipp Kern <pkern@debian.org>
2010-02-08 15:12:04 +0100

    add support for likely/unlikely branch helpers
    gcc can be instructued through __builtin_expect to consider some branches
    more or less likely to be taken.  Add a set of GPLed C compiler attribute
    checks from the xine project as acinclude.m4, check for __builtin_expect
    in configure and add likely and unlikely macros to common.h.
    
    Signed-off-by: Philipp Kern <pkern@debian.org>

--- src/common.h
+++ src/common.h
@@ -1,6 +1,15 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+#include "config.h"
+
+#ifndef SUPPORT__BUILTIN_EXPECT
+# define __builtin_expect(stmt, flag) (stmt)
+#endif
+
+#define likely(x)   __builtin_expect((x),1)
+#define unlikely(x) __builtin_expect((x),0)
+
 typedef int boolean;
 
 #ifndef FALSE
