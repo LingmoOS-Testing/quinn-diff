LingmoOS-Testing/quinn-diff

commit 41ee93c0616023fb79f5b1961560ecbac402c418

src/arch_specific.c: ignore empty lines in P-a-s
Also fix an off-by-one where it included the seperating colon into
the architecture list.

Signed-off-by: Philipp Kern <pkern@debian.org>

src/arch_specific.c
@@ -186,6 +186,17 @@ void read_arch_specific_packages (const char *buffer_p, const long buffer_length
       package_name[k] ='\0';
       debug(debug_as, "package name is \"%s\"", package_name);
 
+      /* Empty line encountered */
+      if(strlen(package_name) == 0)
+      {
+        free(line);
+        free(package_name);
+        continue;
+      }
+
+      /* Skip over the : separator */
+      l++;
+
       /* Architectures is everything after that (excluding comments) */
 
       k = 0;

commit 9efafb25f285fcb241381b8d10bc11ddfd119d97

src/arch_specific.c: apply a syntax check onto P-a-s
Ensure that any non-empty, non-comment line has at least a colon
in it and bail out if this is not the case instead of just
continuing as usual.

This also fixes a subtle off-by-one error introduced in
41ee93c0616023fb79f5b1961560ecbac402c418 which caused quinn-diff
to segfault on lines without any colon.

Signed-off-by: Philipp Kern <pkern@debian.org>

src/arch_specific.c
@@ -16,6 +16,7 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -152,7 +153,8 @@ void read_arch_specific_packages (const char *buffer_p, const long buffer_length
   char *architectures;
   char *package_name;
   long i;
-  unsigned int j, k, l;
+  int j;
+  unsigned int k, l;
 
   check_parseable (buffer_p, buffer_length);
 
@@ -193,13 +195,17 @@ void read_arch_specific_packages (const char *buffer_p, const long buffer_length
         continue;
       }
 
+      if (strlen (line) == l) {
+        fubar(SYSERR, "read_arch_specific_packages: Parse error: %s", line);
+      }
+
       /* Skip over the : separator */
       l++;
 
       /* Architectures is everything after that (excluding comments) */
-
       k = 0;
-      j = strlen (line) - l;
+      j = strlen(line) - (l - 1);
+      assert(j > 0);
       architectures = g_malloc (j + 1);
       while (k < j && line[l] != '#')
 	if (line[l] == '\t')